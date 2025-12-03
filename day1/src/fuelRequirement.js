1d03dd4fc93f23fc1b2936636544c95ee081cd65
Thummala Hem Pradeep Royal <pradeeproyalthummala@Thummalas-MacBook-Pro.local>
2025-12-03 09:45:06 +0530

    part 2 finish

--- day1/src/fuelRequirement.js
+++ day1/src/fuelRequirement.js
@@ -2,11 +2,23 @@ import { masses } from "./parseToArray.js";
 
 const sub = (x, y = 2) => x - y;
 const div = (x, y = 3) => Math.floor(x / y);
-const fuelRequired = (x) => sub(div(x));
 const sum = (x, y) => x + y;
+const fuelRequired = (x) => sub(div(x));
+
+const fuelForMass = (x) => {
+  let fuel = fuelRequired(x);
+  const fuels = [];
+  while (fuel > 0) {
+    if (fuel > 0) {
+      fuels.push(fuel);
+    }
+    fuel = fuelRequired(fuel);
+  }
+  return fuels;
+};
 
 const sumOfFuelRequired = (masses) => {
-  const fuels = masses.map(fuelRequired);
+  const fuels = masses.flatMap(fuelForMass);
   return fuels.reduce(sum, 0);
 };
 
